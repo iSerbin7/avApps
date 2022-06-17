feec4370447ab7579e85b3f2a7edda29e8fbc4c0
iSerbin7 <ru-droid@mail.ru>
2022-06-17 19:18:15 +0300

    Added a new methods

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const router = require('./router/index');
 const PORT = 4000;
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
 const start = async () => {
     try {
-        app.listen(() => console.log(`Server has been started on PORT = ${PORT}`));
+        app.listen(PORT,() => console.log(`Server has been started on PORT = ${PORT}`));
     } catch (e) {
         console.log(`Error started ${e}`);
     }
